ae308667fe463a51dfe3e4f945d6b490560cae38
Rafael Mariano <rafael.silva@arquivei.com.br>
2018-05-07 21:51:49 -0300

    Fix greedy algorithm

--- greedy_algorithm.js
+++ greedy_algorithm.js
@@ -6,23 +6,22 @@ var GreedyAlgorithm = (function() {
   var _colorOptions = ["#F00", "#0F0", "#00F", "#000", "#FF0", "#0FF", "#F0F"]
 
   function greedyColoring() {
-    var result = (new Array(_vertices)).map(() => -1)
     _colours[Object.keys(_graph)[0]] = _colorOptions[0]
 
     var available = new Array(_vertices).fill(true)
 
-    for (var u = 0; u < _vertices; u++) {
+    for (var u = 1; u < _vertices; u++) {
       _counter++
 
       var list = _graph[Object.keys(_graph)[u]]
       for (var i = 0; i < list.length; i++) {
         if (_colours[list[i]]) {
-          available[i] = false
+          available[_colorOptions.indexOf(_colours[list[i]])] = false
         }
       }
 
-      var cr = 0
-      for (; cr < _vertices; cr++) {
+      var cr
+      for (cr = 0; cr < _vertices; cr++) {
         if (available[cr]) {
           break;
         }
