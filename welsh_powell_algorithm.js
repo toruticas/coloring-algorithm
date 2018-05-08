5e7c8d34cae4567e2500768ccd27e374f5944a05
Rafael Mariano <rafael.silva@arquivei.com.br>
2018-05-07 22:15:28 -0300

    Finish welsh-powell algorithm

--- welsh_powell_algorithm.js
+++ welsh_powell_algorithm.js
@@ -3,57 +3,29 @@ var WelshPowellAlgorithm = (function() {
   var _vertices = 0
   var _colours = {}
   var _colorOptions = ["red", "green", "blue", "black"]
-  var _count
-
-  function isSafe(current, option) {
-    for (neighbor of _graph[current]) {
-      if (_colours[neighbor] === option) {
-        return false
-      }
-    }
-
-    return true
-  }
+  var _counter
 
   function algorithm() {
-    var nbColors = 0
-    var allColors = []
+    for (var i = 0; i < _vertices; i++) {
+      if (_colours[Object.keys(_graph)[i]]) {
+        continue
+      } else {
+        _colours[Object.keys(_graph)[i]] = _colorOptions[i]
 
-    _colours[Object.keys(_graph)[0]] = _colorOptions[0]
-
-    for (var i = 0; i < _vertices.length; i++) {
-      allColors.push(i)
+        for (var j = i+1; j < _vertices; j++) {
+          _counter++
+          
+          if (!(Object.keys(_graph)[i].indexOf(Object.keys(_graph)[j]) !== -1) && !_colours[Object.keys(_graph)[j]]) {
+            _colours[Object.keys(_graph)[j]] = _colorOptions[i]
+          } else {
+            continue
+          }
+        }
+      }
     }
-
-    var currentColor = allColors.pop(0)
-    console.log(currentColor);
-    nbColors++
-
-    // while (Object.keys(_graph).length) {
-    //   var index = 0
-    //
-    //   while (index < Object.keys(_graph).length) {
-    //     var conflict = false
-    //     var curretNode = Object.keys(_graph)[index]
-    //
-    //     if (!isSafe(curretNode, currentColor)) {
-    //       conflict = true
-    //     }
-    //
-    //     if (!conflict) {
-    //       _colours[curretNode] = currentColor
-    //       delete _graph[curretNode]
-    //     } else {
-    //       index++
-    //     }
-    //   }
-    //
-    //   currentColor = allColors.pop(0)
-    //   nbColors++
-    // }
   }
 
-  function ordena(graph) {
+  function sortVertices(graph) {
     var newGraph = {}
     var counter = []
 
@@ -73,37 +45,19 @@ var WelshPowellAlgorithm = (function() {
   }
 
   function run(graph) {
-    _graph = ordena(graph)
+    _graph = sortVertices(graph)
     _vertices = Object.keys(graph).length
-    _count = 0
+    _counter = 0
 
-    // console.time('brute_force');
     algorithm()
-    // console.timeEnd('brute_force');
-    // console.log(_count);
-    //
-    // console.log(Object.keys(graph).length);
-    // console.log(Object.keys(_colours).length);
 
-    return _colours
+    return {
+      colours: _colours,
+      counter: _counter,
+    }
   }
 
   return {
     run: run,
   }
 })()
-
-
-// PSEUDO CODE
-// Algoritmo WP(G=(V,E))
-// inicio
-// Ordenar os vértices {x1, x2, ..., xn} tq grau(xi) >= grau(xi+1)
-// para i<-2 até n faça  {inicilizar conjuntos de cores vazios}
-//    Ci <- {}
-// C1 <- {x1} {x1 recebe cor 1}
-// para i <- 2 até n faça
-//    k = Mínimo{j | Adj(xi) Inter Cj = {}, j=0, 1, ..., n
-//    ck = Ck U {xj}
-// fim-para
-// retornar Máximo{ j | Cj <> {}, j=1, 2, ..., n}
-// fim.
